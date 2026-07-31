b1c5102ba26688d11079ab2c8481d2dc339e7023
Nambi Vijay <nambivijay2006@gmail.com>
2026-07-31 19:15:03 +0530

    Add Unique Paths - DP with 1D rolling array

---
+++ arrays/UniquePaths.java
@@ -0,0 +1,20 @@
+/**
+ * Problem: Unique Paths
+ * Count paths from top-left to bottom-right moving only right or down.
+ * Time: O(m*n), Space: O(n)
+ */
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        java.util.Arrays.fill(dp, 1);
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                dp[j] += dp[j - 1];
+        return dp[n - 1];
+    }
+    public static void main(String[] args) {
+        UniquePaths sol = new UniquePaths();
+        System.out.println(sol.uniquePaths(3, 7)); // 28
+        System.out.println(sol.uniquePaths(3, 2)); // 3
+    }
+}
