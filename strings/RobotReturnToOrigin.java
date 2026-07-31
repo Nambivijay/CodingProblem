fc4c020484a2ab59a23356b7708774cdea9ed4f9
Nambi Vijay <nambivijay2006@gmail.com>
2026-07-31 19:24:42 +0530

    Add Robot Return to Origin - Coordinate tracking

---
+++ strings/RobotReturnToOrigin.java
@@ -0,0 +1,22 @@
+/**
+ * Problem: Robot Return to Origin
+ * Check if robot returns to origin after sequence of moves.
+ * Time: O(n), Space: O(1)
+ */
+public class RobotReturnToOrigin {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for (char c : moves.toCharArray()) {
+            if (c == 'U') y++;
+            else if (c == 'D') y--;
+            else if (c == 'L') x--;
+            else if (c == 'R') x++;
+        }
+        return x == 0 && y == 0;
+    }
+    public static void main(String[] args) {
+        RobotReturnToOrigin sol = new RobotReturnToOrigin();
+        System.out.println(sol.judgeCircle("UD")); // true
+        System.out.println(sol.judgeCircle("LL")); // false
+    }
+}
